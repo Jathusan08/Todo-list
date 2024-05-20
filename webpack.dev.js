3c0f7ac1ad1c39ad1cb2f523eca5562b42168699
Jathusan Uthayakumaran <juthayakumara21@hotmail.co.uk>
2024-05-20 01:53:20 +0100

    Add webpack-dev-server and configure development and production server settings
    - Install webpack-dev-server as a development dependency
    - Configure devtool as 'inline-source-map' in webpack.dev.js for better debugging
    - Set up devServer in webpack.dev.js to serve content from ./dist and watch HTML template files
    - Configure devtool as 'source-map' in webpack.prod.js for generating source maps in production

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,4 +3,9 @@ const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
   mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+    watchFiles: ["src/**/*.html"], // Add your HTML template files here
+  },
 });
